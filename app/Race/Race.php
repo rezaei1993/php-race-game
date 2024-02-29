88172a458ad50db871a1745bc458f01d7a46c5cf
rezaei1993 <d.rezaei1993@gmail.com>
2024-02-29 14:42:13 +0330

    Implemented loadVehicles method in Race class.

--- app/Race/Race.php
+++ app/Race/Race.php
@@ -5,8 +5,41 @@ use App\Vehicle\Vehicle;
 
 class Race
 {
-    public function start()
+    private array $vehicles = [];
+    public int $distance = 1000;
+
+    public function __construct()
     {
+        $this->loadVehicles();
+    }
+
+    /**
+     * @return void
+     */
+    private function loadVehicles(): void
+    {
+        $vehiclesData = json_decode(file_get_contents(__DIR__ . '/../../assets/vehicles.json'), true);
+
+        foreach ($vehiclesData as $data) {
+            $vehicle = new Vehicle(
+                $data['name'],
+                $data['maxSpeed'],
+                $data['unit']
+            );
+
+
+            $this->vehicles[] = $vehicle;
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function start(): void
+    {
+        echo "Welcome to Racing Game!\n";
 
     }
+
+
 }
