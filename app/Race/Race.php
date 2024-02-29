138f0eaa86a035f55024328818ae44813bd013b2
rezaei1993 <d.rezaei1993@gmail.com>
2024-02-29 14:40:31 +0330

    Deleted unused method from Race class

--- app/Race/Race.php
+++ app/Race/Race.php
@@ -1,36 +1,12 @@
 <?php
-namespace App\Vehicle;
-class Vehicle
+namespace App\Race;
+
+use App\Vehicle\Vehicle;
+
+class Race
 {
-    public string $name;
-    public float $maxSpeed;
-    public string $unit;
-
-    public function __construct(string $name, float $maxSpeed, string $unit)
+    public function start()
     {
-        $this->name = $name;
-        $this->maxSpeed = $maxSpeed;
-        $this->unit = $unit;
-    }
 
-    /**
-     * @param float $distance
-     * @return float
-     */
-    public function getTimeToComplete(float $distance): float
-    {
-        return $distance / $this->convertMaxSpeedToKm();
-    }
-
-    /**
-     * @return float
-     */
-    public function convertMaxSpeedToKm(): float
-    {
-        return match (strtolower($this->unit)) {
-            'km/h' => $this->maxSpeed,
-            'kts', 'knots' => $this->maxSpeed * 1.85200,
-            default => throw new \InvalidArgumentException("Unknown unit {$this->unit}"),
-        };
     }
 }
