4b5e55bb5b472bb2b51dcaf6d498d98add108590
rezaei1993 <d.rezaei1993@gmail.com>
2024-02-29 14:38:04 +0330

    Set properties in Vehicle class; added convertMaxSpeedToKm method for unit conversion.

--- app/Race/Race.php
+++ app/Race/Race.php
@@ -1,12 +1,36 @@
 <?php
-namespace App\Race;
-
-use App\Vehicle\Vehicle;
-
-class Race
+namespace App\Vehicle;
+class Vehicle
 {
-    public function start()
+    public string $name;
+    public float $maxSpeed;
+    public string $unit;
+
+    public function __construct(string $name, float $maxSpeed, string $unit)
     {
-        
+        $this->name = $name;
+        $this->maxSpeed = $maxSpeed;
+        $this->unit = $unit;
+    }
+
+    /**
+     * @param float $distance
+     * @return float
+     */
+    public function getTimeToComplete(float $distance): float
+    {
+        return $distance / $this->convertMaxSpeedToKm();
+    }
+
+    /**
+     * @return float
+     */
+    public function convertMaxSpeedToKm(): float
+    {
+        return match (strtolower($this->unit)) {
+            'km/h' => $this->maxSpeed,
+            'kts', 'knots' => $this->maxSpeed * 1.85200,
+            default => throw new \InvalidArgumentException("Unknown unit {$this->unit}"),
+        };
     }
 }
--- app/Vehicle/Vehicle.php
+++ app/Vehicle/Vehicle.php
@@ -2,4 +2,27 @@
 namespace App\Vehicle;
 class Vehicle
 {
+    public string $name;
+    public float $maxSpeed;
+    public string $unit;
+
+    public function __construct(string $name, float $maxSpeed, string $unit)
+    {
+        $this->name = $name;
+        $this->maxSpeed = $maxSpeed;
+        $this->unit = $unit;
+    }
+
+
+    /**
+     * @return float
+     */
+    public function convertMaxSpeedToKm(): float
+    {
+        return match (strtolower($this->unit)) {
+            'km/h' => $this->maxSpeed,
+            'kts', 'knots' => $this->maxSpeed * 1.85200,
+            default => throw new \InvalidArgumentException("Unknown unit {$this->unit}"),
+        };
+    }
 }
